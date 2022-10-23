cef33f8fa4a2e2396f8958024a07785c2c25c87b
RoggerK <at76324897@idat.edu.pe>
2022-10-23 18:00:59 -0500

    creo listaactivity

---
+++ app/src/main/java/idat/edu/pe/dm2/grupo01/clinica/ListaActivity.kt
@@ -0,0 +1,11 @@
+package idat.edu.pe.dm2.grupo01.clinica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista)
+    }
+}
