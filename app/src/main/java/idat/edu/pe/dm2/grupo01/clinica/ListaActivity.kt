26737442899119c9a030b817950a9cb922dc3ea6
brian albites <sl73212959@idat.edu.pe>
2022-10-24 16:05:08 -0500

    acabado brrrrr

--- app/src/main/java/idat/edu/pe/dm2/grupo01/clinica/ListaActivity.kt
+++ app/src/main/java/idat/edu/pe/dm2/grupo01/clinica/ListaActivity.kt
@@ -1,4 +1,21 @@
 package idat.edu.pe.dm2.grupo01.clinica
 
-class ListaActivity {
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.R
+import idat.edu.pe.dm2.grupo01.clinica.databinding.ActivityListarinformacionBinding
+
+class ListaActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityListarinformacionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListarinformacionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val listapersonas = intent.getSerializableExtra("listausuario")
+                    as ArrayList<String>
+        val adapter = ArrayAdapter(this, R.layout.simple_list_item_1,
+        listapersonas)
+        binding.lvpersonas.adapter = adapter
+    }
 }
--- app/src/main/java/idat/edu/pe/dm2/grupo01/clinica/RegistroActivity.kt
+++ app/src/main/java/idat/edu/pe/dm2/grupo01/clinica/RegistroActivity.kt
@@ -21,6 +21,50 @@ class RegistroActivity: AppCompatActivity(), OnClickListener, AdapterView.OnItem
         super.onCreate(savedInstanceState)
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnAcceder.setOnClickListener(this)
+        binding.btnLista.setOnClickListener(this)
+        binding.cbdeporte.setOnClickListener(this)
+        binding.cbdibujo.setOnClickListener(this)
+        binding.cbotros.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view is CheckBox){
+            agregarQuitarPreferencias(view)
+        }else{
+            when(view.id){
+                R.id.btnLista -> irLista()
+                R.id.btnAcceder -> registrarPersona()
+            }
+        }
+    }
+
+    private fun registrarPersona(){
+        if(validarFormulario()){
+            var infoPersona = binding.etnombre.text.toString() + " " + "\n" + binding.etapellidos.text.toString() + " " + "\n" +
+                    binding.etdni.text.toString() + " " + "\n" + binding.etcorreo.text.toString() + " " + "\n" + binding.etcontraseA.text.toString() + " " + "\n" +
+                    obtenerGeneroSelccionado() + " " + "\n" + listaPreferencias.toString()
+            listaUsuarios.add(infoPersona)
+            AppMessage.enviarMensaje(binding.root,"Persona correctamente registrada", TypeMessage.SUCCESSFULL)
+            setearFormulario()
+        }
+    }
+
+    private fun setearFormulario(){
+        listaPreferencias.clear()
+        binding.etnombre.setText("")
+        binding.etapellidos.setText("")
+        binding.etdni.setText("")
+        binding.etcorreo.setText("")
+        binding.etcontraseA.setText("")
+        binding.cbdeporte.isChecked = false
+        binding.cbdibujo.isChecked = false
+        binding.cbotros.isChecked = false
+        binding.radioGroup.clearCheck()
+        binding.etnombre.isFocusableInTouchMode = true
+        binding.etnombre.requestFocus()
+
     }
 
     private fun irLista(){
@@ -66,9 +110,9 @@ class RegistroActivity: AppCompatActivity(), OnClickListener, AdapterView.OnItem
     }
 
     fun validarHobbies() : Boolean{
-        var respuesta = true
+        var respuesta = false
         if(binding.cbdeporte.isChecked || binding.cbdibujo.isChecked || binding.cbotros.isChecked){
-            respuesta = false
+            respuesta = true
         }
         return respuesta
     }
@@ -101,24 +145,6 @@ class RegistroActivity: AppCompatActivity(), OnClickListener, AdapterView.OnItem
             respuesta = true
         }
         return respuesta
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
